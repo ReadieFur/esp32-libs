018641e5593d31ca753734d287341ee4c6c2b2b4
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2024-10-21 22:02:00 +0100

    Create StreamDebugger.hpp

---
+++ src/StreamDebugger.hpp
@@ -0,0 +1,34 @@
+//Based on the stream debugger by Volodymyr Shymanskyy.
+
+#include <Stream.h>
+
+class StreamDebugger : public Stream
+{
+public:
+	Stream* DumpStream;
+
+	StreamDebugger(Stream& dataStream, Stream* dumpStream) : _dataStream(dataStream), DumpStream(dumpStream) {}
+
+	virtual size_t write(uint8_t ch)
+	{
+		DumpStream->write(ch);
+		return _dataStream.write(ch);
+	}
+
+	virtual int read()
+	{
+		int ch = _dataStream.read();
+		if (ch != -1 && DumpStream != nullptr)
+			DumpStream->write(ch);
+		return ch;
+	}
+
+	virtual int available() { return _dataStream.available(); }
+
+	virtual int peek() { return _dataStream.peek(); }
+
+	virtual void flush() { _dataStream.flush(); }
+
+private:
+	Stream& _dataStream;
+};
